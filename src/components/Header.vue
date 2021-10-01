d85495a977da9e8d053996e746911d81f24e7804
touros91 <andreageraci91@gmail.com>
2021-10-01 13:40:24 +0200

    Aggiunto struttura base ed header dinamico

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+      <div class="logo">
+          <img :src="logoSrc" :alt="logoAlt">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: ['logoSrc', 'logoAlt']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/common';
+    header {
+        padding: 10px;
+        background-color: $bgColorLight;
+        .logo {
+            max-width: 50px;
+            img {
+                width: 100%;
+            }
+        }
+    }
+</style>
