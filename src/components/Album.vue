3a5690eb3c9805cabbad21cf5b36821de75344e8
touros91 <andreageraci91@gmail.com>
2021-10-01 17:00:02 +0200

    Aggiunto componente Albums - Esercizio completo

---
+++ src/components/Album.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="album">
+        <div class="image">
+            <img :src="card.poster" alt="">
+        </div>
+        <div class="info">
+            <h4 class="title">{{card.title.toUpperCase()}}</h4>
+            <h5 class="artist">{{card.author}}</h5>
+            <h5 class="year">{{card.year}}</h5>      
+        </div>        
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'Album',
+    props: ['card']
+}
+</script>
+
+<style lang="scss" scoped> 
+@import '../assets/style/common';
+
+.album {
+    width: calc(100% / 5 - 30px);
+    margin: 5px 15px;
+    background-color: $bgHeader;
+    text-align: center;
+    color: $mainTextColor;
+    .image{
+        max-width: 130px;
+        margin: 0 auto;
+        padding: 10px;
+        img {
+            width: 100%;
+        }
+    }
+    .info {
+        max-width: 110px;
+        text-align: center;
+        margin: 0 auto;
+        .title {
+            color: white;
+            font-size: 16px;
+            font-weight: 500;
+            margin-bottom: 20px;
+        }
+    }
+}
+</style>
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,25 +1,20 @@
 <template>
     <section>
         <div class="albums-container">
-            <div class="album" v-for="(album, index) in albums" :key="index">
-                <div class="image">
-                    <img :src="album.poster" alt="">
-                </div>
-                <div class="info">
-                    <h4 class="title">{{album.title.toUpperCase()}}</h4>
-                    <h5 class="artist">{{album.author}}</h5>
-                    <h5 class="year">{{album.year}}</h5>      
-                </div>        
-            </div>        
+            <Album v-for="(album, index) in albums" :key="index" :card="album"/>           
         </div>
     </section>
 </template>
 
 <script>
 import axios from 'axios';
+import Album from '../components/Album.vue';
 
 export default {
     name: 'Albums',
+    components: {
+        Album
+    },
     data(){
         return {
             albums: []
@@ -37,43 +32,19 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/style/common';
-    section {
-        height: calc(100vh - 50px);
-        background-color: $bgBody;
-        .albums-container{
-            width: 70%;
-            height: calc(100vh - 50px);
-            margin: 0 auto;
-            display: flex;
-            flex-wrap: wrap;
-            padding: 15px 0; 
-            .album {
-                width: calc(100% / 5 - 30px);
-                margin: 5px 15px;
-                background-color: $bgHeader;
-                text-align: center;
-                color: $mainTextColor;
-                .image{
-                    width: 130px;
-                    margin: 0 auto;
-                    padding: 10px;
-                    img {
-                        width: 100%;
-                    }
-                }
-                .info {
-                    width: 110px;
-                    text-align: center;
-                    margin: 0 auto;
-                    .title {
-                        color: white;
-                        font-size: 16px;
-                        font-weight: 500;
-                        margin-bottom: 20px;
-                    }
-                }
-            }
-        }
-    }
 
+section {
+    height: calc(100vh - 50px);
+    background-color: $bgBody;
+    .albums-container{
+        width: 70%;
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+        margin: auto;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 10px; 
+    }
+}
 </style>
